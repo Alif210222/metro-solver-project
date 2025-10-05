885b1dbf7449a4914e0308b305279fd62b1e6562
Alif-Sarker <sarker202454@gmil.com>
2025-10-05 19:32:54 +0600

    success stories component  added

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () => {
 
 
     return (
-        <div className="navbar bg-[#2c2c46] shadow-sm px-2 md:px-20 py-4  fixed top-0 z-50">
+        <div className="navbar bg-[#1E2532] shadow-sm px-2 md:px-20 py-4  fixed top-0 z-50">
    <div className="navbar-start">
     <div className="dropdown">
       <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden ">
--- src/page/Homepage.jsx
+++ src/page/Homepage.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
+import CardComponent from '../Component/CardComponent';
 
 
 const Homepage = () => {
     return (
         <div className='max-w-7xl mx-auto bg-[#2c2c46]'>
-           this is home page
-
+           <CardComponent></CardComponent>
         </div>
     );
 };
